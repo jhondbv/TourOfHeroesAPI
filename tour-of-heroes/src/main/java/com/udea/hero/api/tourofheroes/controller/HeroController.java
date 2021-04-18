803b9415624ee015f7bbe9a92a25dd3d03d292d4
Jhon_Ballesteros <jhond.b.v@hotmail.com>
2021-04-18 00:22:25 -0500

    Added model and DTO

--- tour-of-heroes/src/main/java/com/udea/hero/api/tourofheroes/controller/HeroController.java
+++ tour-of-heroes/src/main/java/com/udea/hero/api/tourofheroes/controller/HeroController.java
@@ -1,5 +1,7 @@
 package com.udea.hero.api.tourofheroes.controller;
 
+import com.udea.hero.api.tourofheroes.dto.HeroDto;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +18,32 @@ public class HeroController {
         return "Hola mundo";
 
     }
+    
+    public String getHero(int id)
+    {
+        return "hola mundo";
+    }
+    
+    public String getHeroNo404(int id )
+    {
+        return "";
+    }
+
+    public String searchHeroes(String term)
+    {
+        return "";
+    }
+
+    public void UpdateHero(HeroDto hero)
+    {
+
+    }
+
+    public void addHero(HeroDto hero)
+    {}
+
+    public void deletehero(HeroDto hero)
+    {}
+
 
 }
---
+++ tour-of-heroes/src/main/java/com/udea/hero/api/tourofheroes/dto/HeroDto.java
@@ -0,0 +1,21 @@
+package com.udea.hero.api.tourofheroes.dto;
+
+public class HeroDto {
+    
+    public int number;
+    public String name ;
+
+    public int getNumber() {
+        return number;
+    }
+    public void setNumber(int number) {
+        this.number = number;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+   
